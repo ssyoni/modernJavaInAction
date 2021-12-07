3b797bb8b08cc88559e43180e5f6e84664e81875
ssyoni <jsy950601@gmail.com>
2021-12-07 21:16:59 +0900

    add mainClassName

---
+++ src/main/java/com/toy/palace/Application.java
@@ -0,0 +1,11 @@
+package com.toy.palace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
